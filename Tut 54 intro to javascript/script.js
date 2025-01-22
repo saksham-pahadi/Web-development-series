5b70e2599a3ae47ff49c0c50fc0d5ae9f714ba93
saksham-pahadi <saksham-pahadi@users.noreply.github.com>
2025-01-22 21:14:14 +0530

    Intro to JS

---
+++ Tut 54 intro to javascript/script.js
@@ -0,0 +1,16 @@
+alert("Hello world !");
+console.log("Code is running")
+console.log("Code is also running")
+console.log("Code is running like a wow")
+
+var a = prompt("Enter your number")
+var istrue = confirm("are you sure you want to leave this page and blast your computer")
+if(istrue){
+    console.log("Computer is blasting")
+}
+else{
+    console.log("Computer is not blasting")
+}
+console.log("Your number is " + a )
+document.title = "hey i am good"
+// document.body.style.backgroundColor = "red"

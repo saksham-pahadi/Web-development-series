4ce8c6caebcc5a5e0ab4b2ad22bb8b07c4c3cb3f
saksham-pahadi <saksham-pahadi@users.noreply.github.com>
2025-01-22 22:25:08 +0530

    JS Conditionals and operator

---
+++ Tut 56 if, else, else if, else ladder/index.js
@@ -0,0 +1,25 @@
+console.log("hello i am conditional tuturial")
+let age = 18;
+// let grace = 2;
+// age += grace;
+// console.log(age);
+// console.log(age - grace);
+// console.log(age * grace);
+// console.log(age / grace);
+// console.log(age ** grace);
+// console.log(age % grace);
+
+// if else if else ladder
+if((age)>=18){
+    console.log("You can drive");
+}
+else if(age == 0){
+    console.log("Are you kidding");
+}
+else{
+    console.log("You can not drive");
+}
+let a = 6;
+let b = 8;
+let c = a>b?(a-b):(b-a);
+console.log(c);

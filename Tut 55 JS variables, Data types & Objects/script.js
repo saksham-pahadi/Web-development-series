e731784536ead32a0fe905a07881d09cb6688d89
saksham-pahadi <saksham-pahadi@users.noreply.github.com>
2025-01-22 21:48:07 +0530

    Variables Data type & object

---
+++ Tut 55 JS variables, Data types & Objects/script.js
@@ -0,0 +1,35 @@
+console.log("Hey this is tutorial 55")
+var a = 5;
+let b = 6;
+let c = "Harry";
+let _a = "Saksham";
+// console.log(a + b + 8)
+// console.log(typeof a,typeof b,typeof c)
+
+// const a1 = 6;
+// a1 = a1 + 1; Not allowed
+{
+    let a = 66;
+    console.log(a)
+}
+console.log(a)
+
+let x = "Harry bhai";
+let y = 22;
+let z = 3.55;
+const p = true;
+let q = undefined;
+let r = null;
+console.log(x, y, z, p, q, r)
+console.log(typeof x, typeof y, typeof z, typeof p, typeof q, typeof r)
+
+let o = {
+    "name" : "harry",
+    "job code" : 5600,
+    "is handsome":true
+}
+console.log(o);
+o.salary = "100 crores";
+console.log(o);
+o.salary = "500 crores";
+console.log(o);

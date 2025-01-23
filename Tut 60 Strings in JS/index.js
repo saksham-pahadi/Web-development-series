a61dc70facabda39c70f86bbdd2909f3e672513f
saksham-pahadi <saksham-pahadi@users.noreply.github.com>
2025-01-23 21:41:06 +0530

    Strings in JS

---
+++ Tut 60 Strings in JS/index.js
@@ -0,0 +1,22 @@
+console.log("This is string tutorial");
+let a = "Harry";
+console.log(a[0]);
+console.log(a[1]);
+console.log(a[2]);
+console.log(a[3]);
+console.log(a[4]);
+
+console.log(a.length);
+let real_name = "Harry"
+let friend = "Saksham"
+console.log("His name is "+real_name+" and his friend name is "+friend);
+console.log(`His name is ${real_name} and his friend name is ${friend}`);
+let b = "Saksham"
+console.log(b.toUpperCase());
+console.log(b.toLowerCase());
+console.log(b.length);
+console.log(b.slice(1,4));
+console.log(b.slice(1));
+console.log(b.replace("sh","00"));
+console.log(b.concat(a, "Aishwarya","Rahul","Priya"));
+console.log(b);

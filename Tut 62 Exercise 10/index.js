868d9b6b5ebc0e244063d3362b588efafb9b3095
saksham-pahadi <saksham-pahadi@users.noreply.github.com>
2025-01-24 21:20:42 +0530

    Exercise 10

---
+++ Tut 62 Exercise 10/index.js
@@ -0,0 +1,19 @@
+/* create a business name generator by combining list of adjectives and shop name and another words
+
+Adjective:
+Crazy
+Amazing
+Fire
+
+shop name:
+Engine
+Foods
+Garments
+
+Another words:
+Bros
+Limited
+Hub
+
+*/
+

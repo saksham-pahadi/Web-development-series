62c1ed824cc8929232e1e0c567f41e24d4c07315
saksham-pahadi <saksham-pahadi@users.noreply.github.com>
2025-08-05 22:03:32 +0530

    Initialize Next.js project with Tailwind CSS and essential SVG assets
    - Created package.json with Next.js, React, and Tailwind CSS dependencies.
    - Added postcss.config.mjs for Tailwind CSS integration.
    - Included multiple SVG assets in the public directory for use in the application.

---
+++ Tuto 121 Intro to Next js/first/component/Navbar.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div>
+      <nav className='p-4'>
+        <ul className='flex justify-around'>
+            <li>Home</li>
+            <li>Contact</li>
+            <li>About</li>
+        </ul>
+      </nav>
+    </div>
+  )
+}
+
+export default Navbar

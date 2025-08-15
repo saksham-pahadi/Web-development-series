5afb0516bdcf4f403ae82178b590a93546820c20
saksham-pahadi <saksham-pahadi@users.noreply.github.com>
2025-08-16 03:49:01 +0530

    Profile page

--- Tuto 131 Get me a Chai - Patreon clone/get-me-a-chai/app/[username]/page.js
+++ Tuto 131 Get me a Chai - Patreon clone/get-me-a-chai/app/[username]/page.js
@@ -6,20 +6,88 @@ import { useSession, signIn, signOut } from "next-auth/react"
 const Username = ({ Username }) => {
   // let user= params
   const { data: session } = useSession()
-  
+
+  function blockInvalidChar(e) {
+    if (['e', 'E'].includes(e.key)) {
+      e.preventDefault(); // Prevents the 'e' or 'E' from being entered
+    }
+  }
+
   if (!session) {
     redirect('/login')
   }
-  else{
+  else {
 
-  
-  return (
-    <div>
-      <h1>Hey,{Username}</h1>
-      this is profile
-    </div>
-  )
-}
+
+    return (
+      <div className='flex justify-center gap-2 m-2 flex-wrap'>
+
+
+
+        <aside className="aside rounded-lg w-full lg:w-1/4 bg-slate-900 p-5">
+          <h1>Hey,{session.user.name}</h1>
+        </aside>
+
+
+
+
+        <main className='main rounded-lg w-full lg:w-2/3 bg-slate-900 flex p-5 gap-5 flex-wrap justify-evenly'>
+
+          <div className="supporters ">
+            {/* list all supporters as leaderboard */}
+            <h1 className='text-xl font-bold mb-3'>Supporters</h1>
+            <ul>
+              <li className='flex items-center my-4 gap-2'>
+                <lord-icon
+                  src="https://cdn.lordicon.com/hhljfoaj.json"
+                  trigger="hover"
+                  stroke="light"
+                  state="hover-looking-around"
+                  style={{ "width": 33}}>
+                </lord-icon>
+                <span>Subhanm donated <span className='font-bold'>$30</span>  with message "I support you bro. Lots of ❤️"</span>
+              </li>
+              <li className='flex items-center my-4 gap-2'>
+                <lord-icon
+                  src="https://cdn.lordicon.com/hhljfoaj.json"
+                  trigger="hover"
+                  stroke="light"
+                  state="hover-looking-around"
+                  style={{ "width": 33}}>
+                </lord-icon>
+                <span>Krishna donated <span className='font-bold'>$28</span>  with message "I support you bro. Lots of ❤️"</span>
+              </li>
+              <li className='flex items-center my-4 gap-2'>
+                <lord-icon
+                  src="https://cdn.lordicon.com/hhljfoaj.json"
+                  trigger="hover"
+                  stroke="light"
+                  state="hover-looking-around"
+                  style={{ "width": 33}}>
+                </lord-icon>
+                <span>Rahul donated <span className='font-bold'>$43</span>  with message "I support you bro. Lots of ❤️"</span>
+              </li>
+            </ul>
+          </div>
+          <div className="makepayment">
+            <h1 className='text-2xl font-bold mb-3'>Support {session.user.name.split(" ")[0]}</h1>
+
+            <input type='number' className='w-full p-3 rounded-lg bg-slate-800 remove-arrow' placeholder='Enter Amount' onKeyDown={(evt) => (evt.key === '.' || evt.key === '-' || evt.key === '+' || evt.key === 'e' || evt.key === 'E') && evt.preventDefault()} />
+
+
+            {/* or choose from these Amount */}
+            <div className="flex gap-2 mt-2">
+              <button className='bg-slate-800 p-3 rounded-lg'>Donate $10</button>
+              <button className='bg-slate-800 p-3 rounded-lg'>Donate $20</button>
+              <button className='bg-slate-800 p-3 rounded-lg'>Donate $30</button>
+            </div>
+            <textarea className='bg-slate-800 my-2 rounded-lg border-2 border-slate-800 w-full p-2' name="" id="" placeholder='Enter Message'></textarea>
+            <button type="button" className="w-4/5 mx-auto text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br hover:shadow-lg hover:shadow-blue-500/50 hover:dark:shadow-lg hover:dark:shadow-blue-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2 transition-all duration-100 ease-out ">Pay</button>
+          </div>
+        </main>
+      </div>
+    )
+  }
 }
 
 export default Username
--- Tuto 131 Get me a Chai - Patreon clone/get-me-a-chai/components/Navbar.js
+++ Tuto 131 Get me a Chai - Patreon clone/get-me-a-chai/components/Navbar.js
@@ -18,14 +18,14 @@ const Navbar = () => {
 
 
           <button id="dropdownInformationButton" onClick={() => { setshowdropdown(!showdropdown) }}
-          onBlur={() => { setshowdropdown(false) }} data-dropdown-toggle="dropdownInformation" className="flex items-center p-3 rounded-sm text-center bg-gray-800" type="button">  Welcome
+             data-dropdown-toggle="dropdownInformation" className="flex items-center p-3 rounded-sm text-center bg-gray-800" type="button">  Welcome
             <svg className="w-2.5 h-2.5 ms-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
               <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m1 1 4 4 4-4" />
             </svg>
           </button>
 
           {/* <!-- Dropdown menu --> */}
-          <div id="dropdownInformation" onClick={() => { setshowdropdown(!showdropdown) }} className={`z-10 absolute ${showdropdown ? " " : "hidden"} bg-white divide-y divide-gray-100 rounded-lg shadow-sm w-44 dark:bg-gray-700 dark:divide-gray-600  `}>
+          <div id="dropdownInformation" onClick={() => { setshowdropdown(!showdropdown) }}  className={`z-10 absolute ${showdropdown ? " " : "hidden"} bg-white divide-y divide-gray-100 rounded-lg shadow-sm w-44 dark:bg-gray-700 dark:divide-gray-600  `}>
             <div className="px-4 py-3 text-sm text-gray-900 dark:text-white">
 
               <div className="font-medium truncate flex justify-between"> {session.user.name} </div>
@@ -47,8 +47,8 @@ const Navbar = () => {
                 <button >Project</button>
               </Link>
             </ul>
-            <div className="py-2">
-              <button onClick={() => { signOut() }} className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Sign out</button>
+            <div className="py-2 block px-4 hover:bg-gray-100 dark:hover:bg-gray-600 rounded-b-lg ">
+              <button onClick={() => { signOut() }} className="">Sign out</button>
             </div>
           </div>
 
@@ -60,25 +60,26 @@ const Navbar = () => {
       {!session && <>
 
 
-        <div className="hamburger sm:hidden" onBlur={() => { setshowdropdown(false) }}>
+        <div className="hamburger sm:hidden">
 
 
 
-          <button id="dropdownInformationButton" onClick={() => { setshowdropdown(!showdropdown) }}  data-dropdown-toggle="dropdownInformation" className="flex items-center p-3 rounded-sm text-center bg-blue-800" type="button">  Welcome
+          <button id="dropdownInformationButton" onClick={() => { setshowdropdown(!showdropdown) }} data-dropdown-toggle="dropdownInformation" className="flex items-center p-3 rounded-sm text-center bg-blue-800" type="button">  Welcome
             <svg className="w-2.5 h-2.5 ms-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
               <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m1 1 4 4 4-4" />
             </svg>
           </button>
 
           {/* <!-- Dropdown menu --> */}
-          <div id="dropdownInformation" className={`z-10 absolute ${showdropdown ? " " : "hidden"} bg-white divide-y divide-gray-100 rounded-lg shadow-sm w-44 dark:bg-gray-700 dark:divide-gray-600  `}>
+          <div id="dropdownInformation" onClick={() => { setshowdropdown(!showdropdown) }}  className={`z-10 absolute ${showdropdown ? " " : "hidden"} bg-white divide-y divide-gray-100 rounded-lg shadow-sm w-44 dark:bg-gray-700 dark:divide-gray-600  `}>
             <div className="px-4 py-3 text-sm text-gray-900 dark:text-white">
               <div className='font-bold'>Get me a Chai</div>
 
             </div>
             <ul className="py-2 text-sm text-gray-700 dark:text-gray-200" aria-labelledby="dropdownInformationButton">
               <Link href={"/login"} className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 ">
-                <button>Login</button>
+
+                <button >Login</button>
               </Link>
               <Link href={"/"} className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 ">
                 <button >Home</button>
@@ -86,7 +87,7 @@ const Navbar = () => {
               <Link href={"/about"} className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 ">
                 <button >About</button>
               </Link>
-              <Link href={"/project"} className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 ">
+              <Link href={"/projects"} className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 ">
                 <button >Project</button>
               </Link>
 
@@ -110,20 +111,26 @@ const Navbar = () => {
       </Link>
 
       <ul className='hidden sm:flex justify-around items-center gap-4 w-2/5'>
-        <Link href={"/"}>Home</Link>
-        <Link href={"/"}>About</Link>
-        <Link href={"/"}>Projects</Link>
+        <Link className='font-bold hover:shadow-lg hover:shadow-blue-500/50 hover:dark:shadow-lg hover:dark:shadow-blue-800/80 hover:bg-blue-500 p-2 px-3 rounded-lg transition-all delay-5 duration-300 ease-in-out' href={"/"}>Home</Link>
+        <Link className='font-bold hover:shadow-lg hover:shadow-blue-500/50 hover:dark:shadow-lg hover:dark:shadow-blue-800/80 hover:bg-blue-500 p-2 px-3 rounded-lg transition-all delay-5 duration-300 ease-in-out' href={"/about"}>About</Link>
+        <Link className='font-bold hover:shadow-lg hover:shadow-blue-500/50 hover:dark:shadow-lg hover:dark:shadow-blue-800/80 hover:bg-blue-500 p-2 px-3 rounded-lg transition-all delay-5 duration-300 ease-in-out' href={"/projects"}>Projects</Link>
       </ul>
 
 
 
       {session && <>
+        <div className='flex gap-1'>
 
+          <Link href={"/dashboard"} className='hidden sm:flex'>
+            <button className=' text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2'>Dashboard</button>
+          </Link>
 
-        <Link href={`/${session.user.name.split(" ")[0]}`}>
+          <Link href={`/${session.user.name.split(" ")[0]}`}>
 
-          <img className='h-10' srcSet={session.user.image} alt="" />
-        </Link></>}
+            <img className='h-10' srcSet={session.user.image} alt="" />
+          </Link>
+        </div>
+      </>}
 
 
 
